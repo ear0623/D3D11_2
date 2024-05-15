c817ce024902d4dd0515d9683205d3e08df91390
NaMahouse <ear062311@gmail.com>
2024-05-16 00:39:16 +0900

    DepthBuffer 개념 학습. 다음날 정리 필요.

--- vertexshader.hlsl
+++ vertexshader.hlsl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1e6e3c00809eb4c4bb7e60f872ef94b094fded0e8549ebffbfce52ee9df24c2
-size 358
+oid sha256:7dda8960619f7e8e54d7dac4fd5f5abafc579567b889aa8c28f12e70db9ab996
+size 352
